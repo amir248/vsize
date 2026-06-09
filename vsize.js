82640772a7b43393ab6b6b8dd4c9f7a001bdc185
chikchicly <chikchicly@gmail.com>
2026-06-09 16:32:53 +0700

    Your commit message

--- vsize.js
+++ vsize.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const pool = require('./modules/db');
 
-const PORT = 3000;
+const PORT = 3779;
 
 // импортируем email.js модуль
 const mailerRouter = require('./modules/email');
